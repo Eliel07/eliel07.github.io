25ba1e52e9949681649010116dbd3536854dd77c
Eliel07 <eliel.perez@hotmail.com>
2017-02-21 13:53:21 -0600

    Update ejemplo-three.js

--- ejemplo-three.js
+++ ejemplo-three.js
@@ -4,7 +4,7 @@ var escena = new THREE.Scene();
 //Luego se dispone de una cámara, la cual define la perspectiva con que se visualizará la escena.
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
+camara.position.z = 25;
 //El último objeto requerido es un renderizador, el cual genera una imagen, considerando la iluminación y la perspectiva.
 
 var renderer = new THREE.WebGLRenderer();
@@ -19,7 +19,7 @@ renderer.render( escena, camara );
 //Generación y visualización de un objeto^
 var forma = new THREE.BoxGeometry( 1, 1, 1 );
 var cubo  = new THREE.Mesh( forma );
-cubo.rotation.x += 0.8;
+cubo.rotation.x += 0.1;
 cubo.rotation.y += 0.1;
 escena.add( cubo );
 renderer.render( escena, camara );
