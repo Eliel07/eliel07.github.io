a02f27c700f79002f555d48e3c2b8987dc92aaf6
Eliel07 <eliel.perez@hotmail.com>
2017-02-23 14:01:34 -0600

    Update union_malla.js

--- union_malla.js
+++ union_malla.js
@@ -1,4 +1,4 @@
-var troncoForma = new THREE.CylinderGeometry(.5, .5, 1);
+var troncoForma = new THREE.CylinderGeometry( 1, 1, 1);
 var esferaForma = new THREE.SphereGeometry(.65);
 esferaForma.translate(0,1,0);
 
@@ -16,7 +16,7 @@ var escena = new THREE.Scene();
 escena.add(arbolMalla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
+camara.position.z = 50;
 
 renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
