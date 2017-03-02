d5acb416ddf0cb6ce123ebb5ebe1d8867f4bf03b
Eliel07 <eliel.perez@hotmail.com>
2017-03-02 17:56:58 -0600

    Update alfilm.js

--- alfilm.js
+++ alfilm.js
@@ -1,22 +1,22 @@
 var puntos = [];
 for ( var i = 0; i <23; i ++ ) {
 if(i<7){
-    puntos.push( new THREE.Vector2(i,30-i);
+    puntos.push( new THREE.Vector2(i,30-i));
 }
 if(i>6&&i<8){
-    puntos.push( new THREE.Vector2(i-1,30-i);
+    puntos.push( new THREE.Vector2(i-1,30-i));
 }
 if(i>7&&i<12){
-    puntos.push( new THREE.Vector2(14-i,30-i);
+    puntos.push( new THREE.Vector2(14-i,30-i));
 }
 if(i>12&&i<16){
-    puntos.push( new THREE.Vector2(2,30-i);
+    puntos.push( new THREE.Vector2(2,30-i));
 }
 if(i>15&&i<18){
-    puntos.push( new THREE.Vector2(i-13,30-i);
+    puntos.push( new THREE.Vector2(i-13,30-i));
 }
 if(i>17){
-    puntos.push( new THREE.Vector2(22-i,18);
+    puntos.push( new THREE.Vector2(22-i,18));
 }
 }
 
