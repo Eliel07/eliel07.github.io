a8f7a2c987780c968850b6fe28f5d6eb10ca61eb
Eliel07 <eliel.perez@hotmail.com>
2017-03-02 18:07:26 -0600

    Update alfil1.js

--- alfil1.js
+++ alfil1.js
@@ -31,7 +31,7 @@ var escena = new THREE.Scene();
 escena.add(malla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 400;
+camara.position.z = 200;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
