2b8e599043043bb8ff893d15b85ae5bf46d201b4
Eliel07 <eliel.perez@hotmail.com>
2017-03-20 16:01:51 -0600

    Create cruz.js

---
+++ cruz.js
@@ -0,0 +1,35 @@
+var reina = new THREE.Shape();
+
+reina.moveTo(-1,16);
+reina.lineTo(1,16);
+reina.lineTo(1,18);
+reina.lineTo(3, 18);
+reina.lineTo(3, 20);
+reina.lineTo(1, 20);
+reina.lineTo(1, 22);
+reina.lineTo(-1, 22);
+reina.lineTo(-1,20);
+reina.lineTo(-3, 20);
+reina.lineTo(-3, 18);
+reina.lineTo(-1,18);
+reina.lineTo(-1, 16);
+
+
+var forma = new THREE.ExtrudeGeometry( reina,
+                                       {amount: 1} );
+                                       
+var material = new THREE.MeshNormalMaterial();
+var malla = new THREE.Mesh( forma, material );
+//malla.rotateY( Math.PI/4 );
+
+var escena = new THREE.Scene();
+escena.add(malla);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z =60;
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95,
+                      window.innerHeight*.95 );
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
