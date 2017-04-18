00ad8d0c837bcfe20db7bf2fce3191ad76b55708
Eliel07 <eliel.perez@hotmail.com>
2017-04-18 14:10:28 -0500

    Create luzSombras.js

---
+++ luzSombras.js
@@ -0,0 +1,33 @@
+var iluminacion =  new THREE.PointLight(0xFFFFFF);
+iluminacion.position.y=20;
+
+var forma = new THREE.SphereGeometry(1);
+var material = new THREE.MeshLamberMaterial({color: "#00cc00"});
+var malla = new THREE.Mesh(forma,material);
+malla.position.y=2;
+
+vas base = new THREE.Mesh( new THREE.BoxGeometry(5,.1,5),
+                           new THREE.MeshLambertMaterial(
+                           {color:0xFFFFFF}));
+
+var escena = new THREE.Scene();
+escena.add(malla);
+escenal.add(base);
+escena.add(iluminacion);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z=15;
+camara.position.y=5;
+
+var lienzo= document.getElementById("luzSombras");
+var forma = new THREE.WebGLRenderer();
+renderizador.setSize(600,600);
+
+renderizador.shadowMapEnable= true;
+malla.castShadow= true;
+base.receiveShadow= true;
+iluminacion.castShadow= true;
+
+
+renderizador.render(escena,camara);
+
