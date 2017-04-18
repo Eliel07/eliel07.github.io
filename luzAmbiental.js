5fa25f373d88eadcf4dae7f32574bf09a4c9c4cc
Eliel07 <eliel.perez@hotmail.com>
2017-04-18 14:02:36 -0500

    Update luzAmbiental.js

--- luzAmbiental.js
+++ luzAmbiental.js
@@ -12,8 +12,7 @@ var camara = new THREE.PerspectiveCamera();
 camara.position.z=5;
 
 var lienzo= document.getElementById("luzAmbiental");
-var forma = new THREE.WebGLRenderer({canvas: lienzo,
-                                     antialias: true});
+var forma = new THREE.WebGLRenderer();
 
 renderizador.setSize(600,600);
 renderizador.render(escena,camara);
