bd13a42f9ed8cc5e620ad703747cf24cb6e668d9
Eliel07 <eliel.perez@hotmail.com>
2017-06-10 15:25:00 -0500

    Create R6_m.js

--- R6_m.js
+++ R6_m.js
@@ -6,13 +6,13 @@ function listener() {
 
 
 function pushar(e) {
-  if (e.keyCode === 97)
+  if (e.keyCode === 100)
     reyMalla.position.x++;
-  else if (e.keyCode === 122)
+  else if (e.keyCode === 97)
     reyMalla.position.x--;
-    else if (e.keyCode === 115)
+    else if (e.keyCode === 119)
     reyMalla.position.y++;
-    else if (e.keyCode === 120)
+    else if (e.keyCode === 115)
     reyMalla.position.y--;
 }
 
@@ -23,7 +23,7 @@ for (var i=0;i<8;i++)
 {
 for (var j=0;j<8;j++)
 {
-var cubo= new THREE.Mesh( new THREE.BoxGeometry(10,10,-10));
+cubo= new THREE.Mesh( new THREE.BoxGeometry(10,10,-10));
   cubo.position.x =j*10;
   cubo.position.z=-i*10;
   escena.add(cubo);
@@ -56,7 +56,7 @@ if(i==5){    puntos2.push( new THREE.Vector2(1,20));}
 if(i==6){   puntos2.push( new THREE.Vector2(1,22));}
     if(i==7){    puntos2.push( new THREE.Vector2(0,22));}
       if(i==8){    puntos2.push( new THREE.Vector2(0,16));}}
-var cruz = new THREE.LatheGeometry(puntos2);
+cruz = new THREE.LatheGeometry(puntos2);
 
 //CUERPO
 var puntos = [];
@@ -68,7 +68,7 @@ if(i==7){     puntos.push( new THREE.Vector2(4,8));}
 if(i==8){    puntos.push( new THREE.Vector2(2,8));}
 if(i==9){     puntos.push( new THREE.Vector2(4,0));}
     if(i==10){    puntos.push( new THREE.Vector2(0,0));}}
-var cuerpo = new THREE.LatheGeometry(puntos);
+cuerpo = new THREE.LatheGeometry(puntos);
 
 //BASE
 var puntos1 = [];
@@ -76,21 +76,21 @@ for ( var i = 0; i <=22; i ++ ) {
 if(i<=6){     puntos1.push( new THREE.Vector2(i,0));}
 if(i==7){     puntos1.push( new THREE.Vector2(6,-2));}
 if(i==8){    puntos1.push( new THREE.Vector2(0,-2));}}
-var base = new THREE.LatheGeometry(puntos1);
+base = new THREE.LatheGeometry(puntos1);
 
 //UNION
-var cruzMalla = new THREE.Mesh(cruz);
-var cuerpoMalla = new THREE.Mesh(cuerpo);
-var baseMalla = new THREE.Mesh(base);
+cruzMalla = new THREE.Mesh(cruz);
+cuerpoMalla = new THREE.Mesh(cuerpo);
+baseMalla = new THREE.Mesh(base);
 
-var reyForma= new THREE.Geometry();
+reyForma= new THREE.Geometry();
 reyForma.merge(cruzMalla.geometry, cruzMalla.matrix);
 reyForma.merge(cuerpoMalla.geometry, cuerpoMalla.matrix);
 reyForma.merge(baseMalla.geometry, baseMalla.matrix);
 reyForma.translate(10,7,0);
 
-var material = new THREE.MeshNormalMaterial();
-var reyMalla = new THREE.Mesh(reyForma, material);
+material = new THREE.MeshNormalMaterial();
+reyMalla = new THREE.Mesh(reyForma, material);
 reyMalla.position.x=10;
 reyMalla.position.y=10;
 escena.add(reyMalla);
@@ -109,7 +109,8 @@ function loop() {
 }
 
 var malla, camara, renderer, escena;
-
+var reyMalla, material, reyForma;
+var cruzMalla, cuerpoMalla, baseMalla, base,cuerpo, cruz;
 setup();
 loop();
 tablero();
